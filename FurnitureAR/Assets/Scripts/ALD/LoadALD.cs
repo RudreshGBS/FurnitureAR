2476d14e1d3cf8055822a13408724af7f147b2dd
Rudresh <gbs.rudresh@gmail.com>
2021-06-11 17:14:33 +0530

    App created

---
+++ FurnitureAR/Assets/Scripts/ALD/LoadALD.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadALD : MonoBehaviour
+{
+    public ALD ALD;
+    public GameObject ALDCanvas;
+
+    private void Awake()
+    {
+        LoadData();
+        if (!ALD.ALDModel.isvalid) 
+        {
+            ALDCanvas.SetActive(true);
+        }
+    }
+    public void AppQuit() {
+        Application.Quit();
+    }
+
+    void Save()
+    {
+        string json = JsonUtility.ToJson(ALD);
+        Debug.Log(json);
+        File.WriteAllText(Application.dataPath + "/ALD.json", json);
+    }
+    public void LoadData() {
+        StartCoroutine("Load");
+    }
+    public IEnumerator Load()
+    {
+        string url = "https://rudreshgbs.github.io/FurnitureAR/ALD.json";
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+        if (!www.isNetworkError)
+        {
+            Debug.Log(www.downloadHandler.text);
+            string json = www.downloadHandler.text;
+            ALDModel ALData = JsonUtility.FromJson<ALDModel>(json);
+            Debug.Log(ALData.isvalid);
+            this.ALD.ALDModel = ALData;
+        }
+        else
+        {
+            Debug.Log("ERROR: " + www.error);
+        }       
+    }
+
+}
